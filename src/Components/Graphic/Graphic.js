fd04387d2dd765d6ef8499f349c8ffe51638927e
RedYaafte <redyafte@outlook.com>
2018-12-29 20:20:43 -0600

    Date format

--- src/Components/Graphic/Graphic.js
+++ src/Components/Graphic/Graphic.js
@@ -11,7 +11,19 @@ class Graphic extends Component {
     };
 
     axiosCurrency = async () => {
-        let currencyRequest = await axios.get('https://free.currencyconverterapi.com/api/v6/convert?q=USD_MXN,MXN_USD&compact=ultra&date=2018-09-21&endDate=2018-09-26');
+        // Date format
+        let today = new Date();
+        let lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+        let lastWeekYear = lastWeek.getFullYear();
+        let lastWeekMonth = lastWeek.getMonth() + 1;
+        let lastWeekDay = lastWeek.getDate();
+        let lastWeekDate = `${lastWeekYear}-${lastWeekMonth}-${lastWeekDay}`;
+        let todayDate = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+        console.log(todayDate);
+        console.log(lastWeekDate);
+
+        // Conect to api
+        let currencyRequest = await axios.get(`https://free.currencyconverterapi.com/api/v6/convert?q=USD_MXN,MXN_USD&compact=ultra&date=${lastWeekDate}&endDate=${todayDate}`);
         let currency = currencyRequest.data;
         this.setState({ currency });
         console.log(currency);
